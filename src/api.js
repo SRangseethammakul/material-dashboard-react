44004e86d0cc5288424d6febc0be297b9fd56a4a
srangseethammakul <srangseethammakul@gmail.com>
2023-11-01 16:13:28 +0700

    Update

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 const { CancelToken, cancel } = axios.CancelToken.source();
 // Set a base URL for your API, if applicable
-axios.defaults.baseURL = "http://localhost:3012";
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 // Define your Axios instance with custom configuration, if needed
 const instance = axios.create({
