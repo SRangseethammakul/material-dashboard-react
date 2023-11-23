4413701f41f7a89931b499d3b1d35886d006139d
srangseethammakul <srangseethammakul@gmail.com>
2023-11-23 23:36:28 +0700

    Add create user

--- src/layouts/users/index.js
+++ src/layouts/users/index.js
@@ -23,37 +23,73 @@ import MDBox from "components/MDBox";
 import MDButton from "components/MDButton";
 import MDTypography from "components/MDTypography";
 import Modal from "react-modal";
-
+import MDInput from "components/MDInput";
+import Autocomplete from "@mui/material/Autocomplete";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
-import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
+import TextField from "@mui/material/TextField";
 import { instance, cancel } from "../../api";
-// Data
-import authorsTableData from "layouts/users/data/authorsTableData";
-import projectsTableData from "layouts/tables/data/projectsTableData";
-
+const customStyles = {
+  content: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    outline: "none",
+    bgcolor: "background.paper",
+    p: 4,
+  },
+};
 function Tables() {
-  const { columns, rows } = authorsTableData();
-  const { columns: pColumns, rows: pRows } = projectsTableData();
   const [companyTable, setCompanyTable] = React.useState([]);
   const [userTable, setUsers] = React.useState([]);
+  const [companyForCreates, setCompanyForCreates] = React.useState([]);
   const [modalIsOpen, setIsOpen] = React.useState(false);
-  const handleEdit = (data) => {
-    console.log(data);
-    // alert("handleEdit", data);
-    openModal();
-  };
+  const [company, setCompany] = React.useState(null);
+  const [userName, setUserName] = React.useState(null);
+  const [password, setPassword] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [newData, setNewData] = React.useState(false);
   function openModal() {
     setIsOpen(true);
   }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
-  }
-
+  const newUser = () => {
+    openModal();
+  };
+  const saveData = () => {
+    try {
+      if (!userName) {
+        throw new Error("set Username");
+      }
+      if (!password) {
+        throw new Error("set password");
+      }
+      if (!company) {
+        throw new Error("set company");
+      }
+      instance
+        .post("/users/user/create", {
+          username: userName,
+          password: password,
+          companyId: company.internalId,
+        })
+        .then(() => {
+          setNewData(true);
+          closeModal();
+        })
+        .catch((error) => {
+          setError(error.response.data.error.message);
+          console.error("Error fetching data:", error.response.data.error.message);
+        });
+    } catch (error) {
+      setError(error.message);
+    }
+  };
   function closeModal() {
     setIsOpen(false);
   }
@@ -63,6 +99,7 @@ function Tables() {
       .then((response) => {
         setCompanyTable(response.data.companies);
         setUsers(response.data.users);
+        setCompanyForCreates(response.data.companyForCreate);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -74,8 +111,99 @@ function Tables() {
       cancel(); // This cancels the request when the component unmounts
     };
   }, []);
+  React.useEffect(() => {
+    fetchData();
+    return () => {
+      cancel(); // This cancels the request when the component unmounts
+    };
+  }, [newData]);
   return (
     <DashboardLayout>
+      <Modal
+        isOpen={modalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <Card>
+          <MDBox
+            variant="gradient"
+            bgColor="info"
+            borderRadius="lg"
+            coloredShadow="success"
+            mx={2}
+            mt={-3}
+            p={3}
+            mb={1}
+            textAlign="center"
+          >
+            <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+              Create New User
+            </MDTypography>
+            <MDTypography display="block" variant="button" color="white" my={1}>
+              Enter new username, password and company to create
+            </MDTypography>
+          </MDBox>
+          <MDBox pt={4} pb={3} px={3}>
+            <MDBox component="form" role="form">
+              <MDBox mb={2}>
+                <MDInput
+                  type="text"
+                  id="userName"
+                  label="User Name"
+                  variant="standard"
+                  onChange={(e) => {
+                    setUserName(e.target.value);
+                  }}
+                  fullWidth
+                />
+              </MDBox>
+              <MDBox mb={2}>
+                <MDInput
+                  type="password"
+                  id="userPassword"
+                  label="Password"
+                  variant="standard"
+                  onChange={(e) => {
+                    setError(null);
+                    setPassword(e.target.value);
+                  }}
+                  fullWidth
+                />
+              </MDBox>
+              <MDBox mb={2}>
+                {" "}
+                <Autocomplete
+                  options={companyForCreates}
+                  id="autoSelect"
+                  autoSelect
+                  onChange={(event, newValue) => {
+                    setCompany(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField {...params} label="Select Company" variant="standard" />
+                  )}
+                />
+              </MDBox>
+              <MDBox mt={4} mb={1}>
+                <MDButton variant="gradient" onClick={(e) => saveData()} color="info" fullWidth>
+                  sign in
+                </MDButton>
+              </MDBox>
+              {error && (
+                <>
+                  {" "}
+                  <MDBox mt={4} mb={1}>
+                    <MDButton color="danger" fullWidth>
+                      {error}
+                    </MDButton>
+                  </MDBox>
+                </>
+              )}
+            </MDBox>
+          </MDBox>
+        </Card>
+      </Modal>
       <DashboardNavbar />
       <MDBox pt={6} pb={3}>
         <Grid container spacing={6}>
@@ -91,9 +219,18 @@ function Tables() {
                 borderRadius="lg"
                 coloredShadow="info"
               >
-                <MDTypography variant="h6" color="white">
-                  User Table
-                </MDTypography>
+                <Grid container>
+                  <Grid xs={8}>
+                    <MDTypography variant="h6" color="white">
+                      User Table
+                    </MDTypography>
+                  </Grid>
+                  <Grid>
+                    <Grid justifyContent="flex-end" alignItems="stretch">
+                      <MDButton onClick={(e) => newUser(location.id)}>New User</MDButton>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </MDBox>
               <MDBox pt={3}>
                 <DataTable
